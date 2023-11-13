450a192791f12d98f895f27a6fdb6e5bf2d96f02
Youssef Benjelloun <ybkstudies@gmail.com>
2023-11-13 23:11:18 +0100

    ping pong working

---
+++ pingpong/greeter_server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "pingpong/pingpong"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedPingPongServer
+}
+
+func (s *server) Pong(ctx context.Context, in *pb.PingRequest) (*pb.PongResponse, error) {
+	log.Println("[server] received ping")
+	log.Println("[server] sending pong")
+	return &pb.PongResponse{Message: "Hello client" }, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterPingPongServer(s, &server{})
+	log.Printf("[server] listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
