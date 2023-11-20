70884c44c89f5b1f17f74434af7e04e42d2ce513
Youssef Benjelloun <ybkstudies@gmail.com>
2023-11-20 21:11:43 +0100

    script for coping pingpong to g5k

---
+++ copyToG5K.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+set -xe
+
+USER=ybenjellounelkbibi
+SSHKEY=~/.ssh/g5k
+PINGPONG_DIR=~/ensimag-parmake/pingpong
+SITE=grenoble
+
+# copy files and connect
+scp -i $SSHKEY -r $PINGPONG_DIR $USER@access.grid5000.fr:$SITE/
+ssh -i $SSHKEY $USER@access.grid5000.fr
