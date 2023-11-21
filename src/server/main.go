a2497ba598003ccdc4b33ed7b7af00ebab957b8c
Youssef Benjelloun <ybkstudies@gmail.com>
2023-11-21 14:15:30 +0100

    server execuiting remote cmds

---
+++ src/server/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os/exec"
+
+	"google.golang.org/grpc"
+	pb "dismake/proto"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedCommandRemoteExecServer
+}
+
+func (s *server) CmdRemoteExec(ctx context.Context, in *pb.CmdRequest) (*pb.CmdResponse, error) {
+	log.Printf("[server] received: %v\n", in.GetCmd())
+	log.Printf("[server] excuting command %v", in.GetCmd())
+	cmd := exec.Command("bash", "-c", in.GetCmd())
+	stdout, err := cmd.Output()
+	var res string;
+	if err != nil {
+		res = fmt.Sprintf("ERROR: %v", err)
+	} else {
+		res = string(stdout)
+	}
+	return &pb.CmdResponse{Res: res}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterCommandRemoteExecServer(s, &server{})
+	log.Printf("[server] listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
