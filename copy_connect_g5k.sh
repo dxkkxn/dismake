8e1a189507dac7c4003e24ecfca391b27c873212
Guilherme Faccin Huth <guilherme.faccin-huth@grenoble-inp.org>
2023-12-18 11:20:24 +0100

    test perf premier

--- copy_connect_g5k.sh
+++ copy_connect_g5k.sh
@@ -1,14 +1,11 @@
-#!/usr/bin/sh
+#!/bin/sh
 set -xe
 
-USER=ybenjellounelkbibi
+USER=gfaccinhuth
 SSHKEY=~/.ssh/g5k
 SITE=lyon
 
-DISMAKE_DIR=~/ensimag-dismake/dismake
-SCRIPTS_DIR=~/ensimag-dismake/scripts
-MAKEFILES_DIR=~/ensimag-dismake/makefiles
 
 # copy files and connect
-scp -i $SSHKEY -r $DISMAKE_DIR $SCRIPTS_DIR $MAKEFILES_DIR alloc.sh setup.sh make.sh $USER@access.grid5000.fr:$SITE/
+rsync -avz -e "ssh -i $SSHKEY" . $USER@access.grid5000.fr:$SITE/project
 ssh -i $SSHKEY $USER@access.grid5000.fr
