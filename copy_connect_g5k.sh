2c6e461f6ce52a9d9fbbb24d4f5e70dab267a92e
Youssef Benjelloun <ybkstudies@gmail.com>
2023-12-18 13:55:16 +0100

    copy connect

--- copy_connect_g5k.sh
+++ copy_connect_g5k.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -xe
 
-USER=gfaccinhuth
+USER=ybenjellounelkbibi
 SSHKEY=~/.ssh/g5k
 SITE=lyon
 
+DISMAKE_DIR=~/ensimag-dismake/dismake
+MAKEFILES_DIR=~/ensimag-dismake/makefiles
 
 # copy files and connect
-scp -i $SSHKEY -r $DISMAKE_DIR $SCRIPTS_DIR $MAKEFILES_DIR alloc.sh setup.sh make.sh clean.sh $USER@access.grid5000.fr:$SITE/
+scp -i $SSHKEY -r $DISMAKE_DIR $MAKEFILES_DIR alloc.sh setup.sh make.sh clean.sh $USER@access.grid5000.fr:$SITE/
 ssh -i $SSHKEY $USER@access.grid5000.fr
