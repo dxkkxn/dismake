682ddcdc277a02d5cfe743e3e24abe7fdc63e937
essalihn <nabil.es-salihi@grenoble-inp.org>
2023-12-18 22:38:09 +0100

    Plotting for last test

--- metrics_from_csv.py
+++ metrics_from_csv.py
@@ -6,15 +6,23 @@ from collections import defaultdict
 
 if __name__ == "__main__":
     workers = defaultdict(list)
-    with open(sys.argv[1]) as f:
+    filename = sys.argv[1]
+    with open(filename) as f:
         for line in f.readlines():
             worker, time = line.strip().split(', ')
-            workers[worker].append(float(time[:-1]))
+            worker = int(worker)
+            if 'm' in time:
+                minutes = int(time[0])
+                time = float(time[2:-1]) + (minutes * 60)
+            else:
+                time = float(time[:-1])
+            workers[worker].append(time)
     keys = sorted(list(workers.keys()))
     times = [sum(workers[key])/len(workers[key]) for key in keys]
     plt.plot(keys, times, marker='o')
     plt.xlabel('Number of workers')
     plt.ylabel('Time (s)')
     plt.title('Time execution per number of worker')
-    plt.savefig("workers")
+    filename = filename[:-4]
+    plt.savefig(filename)
     plt.close()
