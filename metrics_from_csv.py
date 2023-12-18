35bdc9862e153d0c68bde410f21e996da54d8691
Youssef Benjelloun <ybkstudies@gmail.com>
2023-12-18 18:47:48 +0100

    metrics_from_csv: added average

--- metrics_from_csv.py
+++ metrics_from_csv.py
@@ -2,16 +2,17 @@
 
 import matplotlib.pyplot as plt
 import sys
+from collections import defaultdict
 
 if __name__ == "__main__":
+    workers = defaultdict(list)
     with open(sys.argv[1]) as f:
-        workers = []
-        times = []
         for line in f.readlines():
             worker, time = line.strip().split(', ')
-            workers.append(int(worker))
-            times.append(float(time[:-1]))
-    plt.plot(workers, times, marker='o')
+            workers[worker].append(float(time[:-1]))
+    keys = sorted(list(workers.keys()))
+    times = [sum(workers[key])/len(workers[key]) for key in keys]
+    plt.plot(keys, times, marker='o')
     plt.xlabel('Number of workers')
     plt.ylabel('Time (s)')
     plt.title('Time execution per number of worker')
